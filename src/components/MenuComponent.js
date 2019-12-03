b7b785cdee7f2350312b6ceb8ab9c6e940526bca
natali <qwemee@gmail.com>
2019-12-04 00:46:41 +0700

    Creating a home page

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './MenuComponent.module.css';
+
+class MenuComponent extends React.Component {
+	render() {
+		return <>
+				<div className={styles.head}>
+				<div className={styles.header__logo}>AVOCADO</div>
+					<nav>
+						<a className={styles.nav__link} href="#">About</a>
+						<a className={styles.nav__link} href="#">Add Product</a>
+						<a className={styles.nav__link} href="#">Statistic</a>
+						<a className={styles.nav__link} href="#">Config</a>
+					</nav>	
+				</div>
+				<div className={styles.line}></div>	</>;
+	}
+}
+
+export default MenuComponent;
